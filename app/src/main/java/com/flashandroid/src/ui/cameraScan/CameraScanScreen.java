4850e2e3ccee0c96c18ee9deb839621b19f6edbf
Sridhar Prasath <sridharprasath@dynamicelement.com>
2025-03-21 10:41:05 +0100

    Add controls for flash and zoom

--- app/src/main/java/com/flashandroid/src/ui/cameraScan/CameraScanScreen.java
+++ app/src/main/java/com/flashandroid/src/ui/cameraScan/CameraScanScreen.java
@@ -1,15 +1,12 @@
 package com.flashandroid.src.ui.cameraScan;
 
-import static android.content.Context.VIBRATOR_SERVICE;
 import static com.flashandroid.src.ui.common.Constants.DEFAULT_ZOOM_BUTTON_VISIBLE;
-
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
@@ -18,15 +15,16 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.flashandroid.R;
 import com.flashandroid.databinding.CameraScanScreenBinding;
+import com.flashandroid.src.ui.common.VibrationModel;
 
 
 public class CameraScanScreen extends Fragment {
     private CameraScanScreenBinding binding;
-    protected AnimationDrawable uploadingAnimation;
     protected boolean flashState;
     protected boolean isZoomButtonVisible = DEFAULT_ZOOM_BUTTON_VISIBLE;
-    protected Vibrator vibrator;
+    private VibrationModel vibrationModel;
     private CameraScanModel cameraScanModel;
 
     @Override
@@ -49,6 +47,7 @@ public class CameraScanScreen extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         cameraScanModel = new CameraScanModel(this.getContext());
+        this.vibrationModel = new VibrationModel(requireActivity());
         initializeLayout();
 
         cameraScanModel.initCamera(binding.cameraView);
@@ -56,8 +55,53 @@ public class CameraScanScreen extends Fragment {
 
         flashState = cameraScanModel.getSavedFlash();
         binding.cameraView.changeFlashState(flashState);
+        binding.flashButton.setImageResource(flashState ? R.drawable.ic_flash_on : R.drawable
+                .ic_flash_off);
+        binding.zoomSeekbar.setProgress(binding.cameraView.getCurrentZoom());
+        binding.zoomSeekbar.setMin(1);
+        binding.zoomSeekbar.setMax(binding.cameraView.getMaxZoom());
         binding.cameraView.changeZoomLevel(binding.cameraView.getCurrentZoom());
-        binding.cameraView.setOnClickListener(v -> binding.cameraView.focusCamera());
+        binding.zoomSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                binding.cameraView.changeZoomLevel(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                isZoomButtonVisible = true;
+                binding.zoomSeekbar.setProgress(seekBar.getProgress());
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                isZoomButtonVisible = false;
+                binding.zoomSeekbar.setVisibility(View.INVISIBLE);
+                binding.zoomSeekbar.setEnabled(false);
+                Toast.makeText(requireActivity(),
+                        getString(R.string.camera_scan_zoom_level) +
+                                " " + seekBar.getProgress() + "/" + seekBar.getMax(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        this.binding.cameraView.setOnClickListener(v -> binding.cameraView.focusCamera());
+
+        this.binding.flashButton.setOnClickListener(view14 -> {
+            this.vibrationModel.createVibration();
+            this.binding.flashButton.setImageResource(flashState ? R.drawable.ic_flash_off : R.drawable
+                    .ic_flash_on);
+            this.binding.cameraView.changeFlashState(!flashState);
+            this.flashState = !flashState;
+        });
+
+        binding.zoomButton.setOnClickListener(view12 -> {
+            this.vibrationModel.createVibration();
+            this.binding.zoomButton.setImageResource(isZoomButtonVisible ? R.drawable.ic_zoom : R
+                    .drawable.ic_baseline_zoom_in_24);
+            this.binding.zoomSeekbar.setVisibility(isZoomButtonVisible ? View.INVISIBLE : View.VISIBLE);
+            this.binding.zoomSeekbar.setEnabled(!isZoomButtonVisible);
+            this.isZoomButtonVisible = !isZoomButtonVisible;
+        });
 
 
     }
@@ -81,17 +125,14 @@ public class CameraScanScreen extends Fragment {
      */
     @RequiresApi(api = Build.VERSION_CODES.P)
     protected void initializeLayout() {
-        vibrator = (Vibrator) requireActivity().getSystemService(VIBRATOR_SERVICE);
+        binding.zoomSeekbar.setVisibility(View.INVISIBLE);
     }
 
 
     protected void handleException(Exception exception) {
         requireActivity().runOnUiThread(() -> {
-            uploadingAnimation.stop();
             Toast.makeText(CameraScanScreen.this.requireActivity(),
                     exception.getMessage(), Toast.LENGTH_SHORT).show();
         });
-//        NavigationService.CameraNav.moveToModeSelectView(Objects.requireNonNull(getView()), userMode,
-//                createCollectionSelected, mddiCid);
     }
 }
--- app/src/main/java/com/flashandroid/src/ui/common/Constants.java
+++ app/src/main/java/com/flashandroid/src/ui/common/Constants.java
@@ -2,4 +2,6 @@ package com.flashandroid.src.ui.common;
 public interface Constants {
     boolean DEFAULT_TOGGLE_FLASH = true;
     boolean DEFAULT_ZOOM_BUTTON_VISIBLE = false;
+
+    int VIBRATION_MS = 60;
 }
